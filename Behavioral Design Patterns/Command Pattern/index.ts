78e36a706db914dea94f607a4edb49dab17f3dd5
codedinsound <107017540+codedinsound@users.noreply.github.com>
2022-11-01 19:51:22 -0600

    1 file modified

--- Behavioral Design Patterns/Command Pattern/index.ts
+++ Behavioral Design Patterns/Command Pattern/index.ts
@@ -16,6 +16,59 @@
  *   - bacause of the abstract layer we can achieve loosely coupled design
  *   - the user interface components know nothing about the underlaying
  *   - and we can change the sender's behaviour at run-time.
- * 
+ *
  *   - change
  */
+
+interface Command {
+  execute(): void;
+}
+
+// Receiver
+class Light {
+  public turnOn() {
+    console.log('Lights On.......');
+  }
+
+  public turnOff() {
+    console.log('Lights Off......');
+  }
+}
+
+class TurnOnCommand implements Command {
+  private light: Light;
+
+  constructor(light: Light) {
+    this.light = light;
+  }
+
+  execute(): void {
+    this.light.turnOn();
+  }
+}
+
+class TurnOffCommand implements Command {
+  private light: Light;
+
+  constructor(light: Light) {
+    this.light = light;
+  }
+
+  execute(): void {
+    this.light.turnOff();
+  }
+}
+
+class Switcher {
+  public commands: Map<Command, Command>;
+
+  constructor() {
+    this.commands = new Map<Command, Command>();
+  }
+
+  public executeCommands(): void {
+    for (const command in this.commands.values()) {
+      command.execute();
+    }
+  }
+}
